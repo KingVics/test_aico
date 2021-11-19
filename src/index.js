46a890e7c44b9d9c21053991e26f0d208508ed95
Okoromi Victor <okoromivictorsunday@gmail.com>
2021-11-19 22:25:40 +0100

    Coming soon added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import ComingSoon from './ComingSoon';
+import ComingSoon from  './coming/ComingSoon';
 import {Provider} from "react-redux"
 import {compose, applyMiddleware, createStore} from "redux"
 import thunk from "redux-thunk"
